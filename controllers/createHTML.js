4388c4051c4453d99dac4f0f5e64d7f09b221a5b
Mariano <mariano_farace@hotmail.com>
2022-05-22 16:47:36 +0200

    Change to paragraph

--- controllers/createHTML.js
+++ controllers/createHTML.js
@@ -1,6 +1,6 @@
 const createHTML = (recipientName, emitterName, message) => {
   const htmlContent = `
-<h1>Hi ${recipientName}, ${emitterName} has sent you this message:</h1>
+<p>Hi ${recipientName}, ${emitterName} has sent you this message:</p>
 <p>${message}</p>
 `;
   return htmlContent;
