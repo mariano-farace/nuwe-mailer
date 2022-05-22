b0fccdf40da5cadf7f2a2051b5cc6232b455469d
Mariano <mariano_farace@hotmail.com>
2022-05-22 15:44:40 +0200

    Add comment

--- controllers/OAuth.js
+++ controllers/OAuth.js
@@ -6,8 +6,9 @@ const {
   CLIENT_SECRET, //No issue
   REDIRECT_URI,
   REFRESH_TOKEN,
-  USER_MAIL,
 } = require("../config");
+
+//TODO confirmar que esto no vaya a generar error cuando expire el regresh token despues de un dia! sino lo vas a tener que transformar en una funcion y que se pueda llamar cada vez que mandas un mail para crear oauth2Client
 
 const oauth2Client = new google.auth.OAuth2(
   CLIENT_ID,
