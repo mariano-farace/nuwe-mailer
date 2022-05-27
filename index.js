ab90a4f2fae8287bf2e9b8664d8cd556e0d15208
Mariano <mariano_farace@hotmail.com>
2022-05-27 16:00:56 +0200

    Modify TODO's

--- index.js
+++ index.js
@@ -16,8 +16,6 @@ app.use(express.static(path.join(__dirname, "public")));
 // TODO darle publish app en la consola de google. Poner un logo
 // TODO testear los errores, por ejemplo el de cuando expira el token
 // TODO borrar todos los TODO
-// TODO fijate como vas a manejar lo del trnasporter y oauth client, si va a ser una funcion, o exportado, y si eso genera problemas con expired token. // TODO considerar si mandar directamente el transporter yque sea el mismo para las dos rutas que mandan mails, puede dar problemas con la caducacion de refresh y acces token
-// TODO hacer que el refresh token se pida solo???
 
 app.listen(process.env.PORT || 3030, () => {
   console.log("Server started on port 3030"); // TODO poner el port bien aca
