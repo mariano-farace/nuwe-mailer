628766a7fffe586f654d8cf4544ad7feed9579a6
Mariano <mariano_farace@hotmail.com>
2022-05-27 10:44:51 +0200

    Rename route

--- routes/mail.js
+++ routes/mail.js
@@ -8,6 +8,6 @@ const {
 // TODO hacer que el refresh token se pida solo???
 // TODO less secure app va a dejar de funcionar pronto!
 // TODO darle publish app en la consola de google. Poner un logo
-router.post("/send-email", sendPeerToPeerMail);
+router.post("/email-simple", sendPeerToPeerMail);
 router.post("/register", sendWelcomeMail);
 module.exports = router;
