befa4df6f77943040bc235f961960ec52950f2cf
Mariano <mariano_farace@hotmail.com>
2022-05-27 11:55:44 +0200

    Add todo

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ app.use(express.static(path.join(__dirname, "public")));
 // TODO eliminar el front?
 // TODO HACer tests?
 // TODO crear template de env
+// TODO Organizar en subcarpetas
+// TODO Fijate que los templates que estas usando de los mails sean una cosa handwriten o algo de nodemailer
+// TODO remove all console.log
+// TODO less secure app va a dejar de funcionar pronto!
+// TODO darle publish app en la consola de google. Poner un logo
+// TODO testear los errores, por ejemplo el de cuando expira el token
+
 app.listen(process.env.PORT || 3030, () => {
   console.log("Server started on port 3030"); // TODO poner el port bien aca
 });
