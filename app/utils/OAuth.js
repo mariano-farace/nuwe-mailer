5acb9fcffc07550d7c65e41de7d7a47d3a23c61a
Mariano <mariano_farace@hotmail.com>
2022-05-27 12:34:19 +0200

    Set credentials on transporter creation

--- app/utils/OAuth.js
+++ app/utils/OAuth.js
@@ -3,7 +3,6 @@ const {
   CLIENT_ID, // No issue
   CLIENT_SECRET, // No issue
   REDIRECT_URI,
-  REFRESH_TOKEN,
 } = require("../config");
 
 const oauth2Client = new google.auth.OAuth2(
@@ -11,9 +10,5 @@ const oauth2Client = new google.auth.OAuth2(
   CLIENT_SECRET,
   REDIRECT_URI
 );
-
-oauth2Client.setCredentials({
-  refresh_token: REFRESH_TOKEN,
-});
 
 module.exports = { oauth2Client };
--- app/utils/nodeMailerTransporter.js
+++ app/utils/nodeMailerTransporter.js
@@ -8,6 +8,10 @@ const nodemailer = require("nodemailer");
 const { oauth2Client } = require("./OAuth");
 
 const createNodeMailerTransporter = async () => {
+  oauth2Client.setCredentials({
+    refresh_token: REFRESH_TOKEN,
+  });
+
   const accessToken = await oauth2Client.getAccessToken();
 
   const transporter = nodemailer.createTransport({
