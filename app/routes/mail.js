8bad88b767d59883ef757c6f904fa5701293c2d0
Mariano <mariano_farace@hotmail.com>
2022-05-29 13:22:05 +0200

    Add documentation

--- app/routes/mail.js
+++ app/routes/mail.js
@@ -21,7 +21,7 @@ const {
  * POST /email-simple
  * @summary Send an email to specified email address
  * @tags mail
- * @param {emailData} request.body.required - The email data
+ * @param {emailData} request.body.required - The recipient, emitter and mail data
  * @return {object} 200 - success response - application/json
  * @return {object} 400 - Bad request response
  * @return {object} 500 - Internal server error response
@@ -62,5 +62,53 @@ const {
    }
  */
 router.post("/email-simple", sendPeerToPeerMail);
+
+/**
+ * Email data
+ * @typedef {object} emailData
+ * @property {name} name.required - The name of the user to whom welcome email will be sent
+ * @property {email} email.required - User email
+ */
+
+/**
+ * POST //register
+ * @summary Send welcome email to specified email address
+ * @tags mail
+ * @param {emailData} request.body.required - The user data
+ * @return {object} 200 - success response - application/json
+ * @return {object} 400 - Bad request response
+ * @return {object} 500 - Internal server error response
+ * @example request - Payload example
+ * {
+    "name": "John Doe",
+    "mail":"johnDoe@mail.com"
+   }
+ *
+ * @example response - 200 - Example success response
+ * { 
+ *   "status":"sent", 
+ *   "info": {
+        "accepted": [
+            "johnDoe@mail.com"
+        ],
+        "rejected": [],
+        "envelopeTime": 306,
+        "messageTime": 584,
+        "messageSize": 975,
+        "response": "250 2.0.0 OK  1653647757 x5-20020a05600c21c500b0039787538c9csm1889589wmj.20 - gsmtp",
+        "envelope": {
+            "from": "serviceAccount@gmail.com",
+            "to": [
+                "johnDoe@mail.com"
+            ]
+        },
+        "messageId": "<91952e43-cc65-36e1-3d03-f82980d0ed99@gmail.com>"
+    }}
+ * @example response - 400 - Example error response
+ * {
+    "status:": "Validation error",
+    "message": "\"name\" is required"
+   }
+ */
 router.post("/register", sendWelcomeMail);
 module.exports = router;
