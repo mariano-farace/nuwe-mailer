5ba9beedfb432021483c6fae6f717b4b1784403a
Mariano <mariano_farace@hotmail.com>
2022-05-31 10:37:59 +0200

    Clean up

--- index.js
+++ index.js
@@ -9,12 +9,6 @@ app.use(bodyParser.json());
 app.use("/", mailRouter);
 app.use(express.static(path.join(__dirname, "app/public")));
 
-// TODO probar que pasa si sacas less secure apps!!!
-// TODO eliminar el front?
-// TODO PONER ESO DE CALIDAD DEL CODIGO
-// TODO darle publish app en la consola de google. Poner un logo
-// TODO borrar todos los TODO
-
 app.listen(process.env.PORT || 3030, () => {
-  console.log("Server started on port 3030"); // TODO poner el port bien aca
+  console.log("Server started on port 3030");
 });
